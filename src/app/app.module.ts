e86ea903fdd9eb70699a9e643b69ada2e121a637
Romain Jeannoutot <romain.jeannoutot@gmail.com>
2018-05-10 00:19:00 +0200

    implement angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+import { AppAngularMaterialModule } from "./app-angular-material.module";
 
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "../components/home/home.component";
@@ -21,6 +24,8 @@ const ROUTES: Routes = [{
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    AppAngularMaterialModule,
     RouterModule.forRoot(ROUTES)
   ],
   providers: [],
