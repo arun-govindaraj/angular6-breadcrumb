6ef59f6592ef5f9b5a435e95735da792626fbbd4
Romain Jeannoutot <romain.jeannoutot@gmail.com>
2018-05-09 23:01:59 +0200

    implement angular router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,18 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "../components/home/home.component";
+
+const ROUTES: Routes = [{
+  path: "home",
+  component: HomeComponent
+}, {
+  path: "",
+  redirectTo: "/home",
+  pathMatch: "full"
+}];
 
 @NgModule({
   declarations: [
@@ -10,7 +20,8 @@ import { HomeComponent } from "../components/home/home.component";
     HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
