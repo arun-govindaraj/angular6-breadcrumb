efe8db72e52e40d17c047639e22da3dc4ba7831a
Romain Jeannoutot <romain.jeannoutot@gmail.com>
2018-05-10 01:08:59 +0200

    listen for router changes and display breadcrumbs informations on breadcrumbs component

--- src/components/breadcrumbs/breadcrumbs.component.ts
+++ src/components/breadcrumbs/breadcrumbs.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
+import { ActivationEnd, Router } from "@angular/router";
+
+import { Breadcrumb } from "../../models/breadcrumb";
 
 @Component({
   selector: "app-breadcrumbs",
   templateUrl: "./breadcrumbs.component.html",
   styleUrls: ["./breadcrumbs.component.scss"]
 })
-export class BreadcrumbsComponent implements OnInit {
+export class BreadcrumbsComponent {
 
-  constructor() { }
+  breadcrumbs: Breadcrumb[] = [];
 
-  ngOnInit() {
+  constructor(private router: Router) {
+    this.router.events.subscribe(e => {
+      if (e instanceof ActivationEnd && e.snapshot.data.breadcrumbs) {
+        this.breadcrumbs = e.snapshot.data.breadcrumbs;
+      }
+    });
   }
 
 }
