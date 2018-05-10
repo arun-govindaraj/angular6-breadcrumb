d08564c215031168c95431bcfbd80e6e60fbe23b
Romain Jeannoutot <romain.jeannoutot@gmail.com>
2018-05-11 01:16:37 +0200

    add breadcrumb via provider on init category component

--- src/components/category/category.component.ts
+++ src/components/category/category.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+
+import { BreadcrumbProvider } from "../../providers/breadcrumb";
+
+import { Breadcrumb } from "../../models/breadcrumb";
+
+import { CATEGORIES } from "../../mocks/categories";
 
 @Component({
   selector: "app-category",
@@ -7,9 +14,10 @@ import { Component, OnInit } from "@angular/core";
 })
 export class CategoryComponent implements OnInit {
 
-  constructor() { }
+  constructor(private breadcrumbProvider: BreadcrumbProvider, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
+    this.breadcrumbProvider.addItem(new Breadcrumb(CATEGORIES.get(this.route.snapshot.params.name), this.router.url));
   }
 
 }
