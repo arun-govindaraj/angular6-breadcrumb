2c2172db80ec127f24fd9b63691099a3d6fc9733
Romain Jeannoutot <romain.jeannoutot@gmail.com>
2018-05-11 18:18:52 +0200

    use category provider instead of categories maps in category component

--- src/components/category/category.component.ts
+++ src/components/category/category.component.ts
@@ -2,22 +2,26 @@ import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
 
 import { BreadcrumbProvider } from "../../providers/breadcrumb";
+import { CategoryProvider } from "../../providers/category";
 
 import { Breadcrumb } from "../../models/breadcrumb";
-
-import { CATEGORIES } from "../../mocks/categories";
 
 @Component({
   selector: "app-category",
   templateUrl: "./category.component.html",
-  styleUrls: ["./category.component.scss"]
+  styleUrls: ["./category.component.scss"],
+  providers: [CategoryProvider]
 })
 export class CategoryComponent implements OnInit {
 
-  constructor(private breadcrumbProvider: BreadcrumbProvider, private route: ActivatedRoute, private router: Router) { }
+  constructor(private breadcrumbProvider: BreadcrumbProvider,
+              private categoryProvider: CategoryProvider,
+              private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
-    this.breadcrumbProvider.addItem(new Breadcrumb(CATEGORIES.get(this.route.snapshot.params.name), this.router.url));
+    this.categoryProvider.getNameByUrl(this.route.snapshot.params.name)
+      .then(name => this.breadcrumbProvider.addItem(new Breadcrumb(name, this.router.url)));
   }
 
 }
