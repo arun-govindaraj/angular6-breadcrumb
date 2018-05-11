589c4050093f440d2579c7e0ea1c3e3374f14b6b
Romain Jeannoutot <romain.jeannoutot@gmail.com>
2018-05-11 18:12:47 +0200

    use recipe provider instead of recipes maps in recipe component

--- src/components/recipe/recipe.component.ts
+++ src/components/recipe/recipe.component.ts
@@ -2,22 +2,26 @@ import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
 
 import { BreadcrumbProvider } from "../../providers/breadcrumb";
+import { RecipeProvider } from "../../providers/recipe";
 
 import { Breadcrumb } from "../../models/breadcrumb";
-
-import { RECIPES } from "../../mocks/recipes";
 
 @Component({
   selector: "app-recipe",
   templateUrl: "./recipe.component.html",
-  styleUrls: ["./recipe.component.scss"]
+  styleUrls: ["./recipe.component.scss"],
+  providers: [RecipeProvider]
 })
 export class RecipeComponent implements OnInit {
 
-  constructor(private breadcrumbProvider: BreadcrumbProvider, private route: ActivatedRoute, private router: Router) { }
+  constructor(private breadcrumbProvider: BreadcrumbProvider,
+              private recipeProvider: RecipeProvider,
+              private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
-    this.breadcrumbProvider.addItem(new Breadcrumb(RECIPES.get(this.route.snapshot.params.name), this.router.url));
+    this.recipeProvider.getNameByUrl(this.route.snapshot.params.name)
+      .then(name => this.breadcrumbProvider.addItem(new Breadcrumb(name, this.router.url)));
   }
 
 }
